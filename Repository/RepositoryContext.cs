94822f2df2c8e1843a03e1fa293fa36c92df81b1
FlyFu-0 <tihonov-04@yandex.ru>
2024-09-26 18:09:33 +0300

    db context, factoryContext

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+public class RepositoryContext : DbContext
+{
+	public RepositoryContext(DbContextOptions options) : base(options)
+	{ }
+
+	public DbSet<Post>? Posts { get; set; }
+	public DbSet<Like>? Likes { get; set; }
+	public DbSet<Tag>? Tags { get; set; }
+	public DbSet<PostTag>? PostTags { get; set; }
+	public DbSet<Comment>? Comments { get; set; }
+}
