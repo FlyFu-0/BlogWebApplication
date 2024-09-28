48d91f7d69986bbb554a7453d90178a16844e1e3
FlyFu-0 <tihonov-04@yandex.ru>
2024-09-29 00:07:13 +0300

    add service in DI

--- BlogWebApplication/Extensions/ServiceExtensions.cs
+++ BlogWebApplication/Extensions/ServiceExtensions.cs
@@ -2,6 +2,8 @@
 using LoggerService;
 using Microsoft.EntityFrameworkCore;
 using Repository;
+using Service;
+using Service.Contracts;
 
 namespace BlogWebApplication.Extensions;
 
@@ -25,6 +27,8 @@ public static class ServiceExtensions
 	public static void ConfigureRepositoryManager(this IServiceCollection services) =>
 		services.AddScoped<IRepositoryManager, RepositoryManager>();
 
+	public static void ConfigureServiceManager(this IServiceCollection services) =>
+		services.AddScoped<IServiceManager, ServiceManager>();
 	public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
 		services.AddDbContext<RepositoryContext>(opts => opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 }
