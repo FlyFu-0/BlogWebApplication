2fb99bdac8c4d7bedd5d43b42d9968836b8b33b3
FlyFu-0 <tihonov-04@yandex.ru>
2024-09-30 16:02:10 +0300

    add user in servicemanager

--- Service/ServiceManager.cs
+++ Service/ServiceManager.cs
@@ -10,6 +10,7 @@ public sealed class ServiceManager : IServiceManager
 	private readonly Lazy<ITagService> _tagService;
 	private readonly Lazy<ILikeService> _likeService;
 	private readonly Lazy<ICommentService> _commentService;
+	private readonly Lazy<IUserService> _userService;
 
 	public ServiceManager(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
 	{
@@ -17,6 +18,7 @@ public sealed class ServiceManager : IServiceManager
 		_tagService = new Lazy<ITagService>(() => new TagService(repository, logger, mapper));
 		_likeService = new Lazy<ILikeService>(() => new LikeService(repository, logger, mapper));
 		_commentService = new Lazy<ICommentService>(() => new CommentService(repository, logger, mapper));
+		_userService = new Lazy<IUserService>(() => new UserService(repository, logger, mapper));
 	}
 
 	public IPostService PostService => _postService.Value;
@@ -26,4 +28,6 @@ public sealed class ServiceManager : IServiceManager
 	public ILikeService LikeService => _likeService.Value;
 
 	public ICommentService CommentService => _commentService.Value;
+
+	public IUserService UserService => _userService.Value;
 }
